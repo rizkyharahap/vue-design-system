dc38676c181bacdd01619e388216b38d6389617d
rizkyharahap <rizkyhh1999@gmail.com>
2024-12-30 00:11:13 +0700

    test(button): add snapshot test

--- src/components/button/__tests__/Button.spec.ts
+++ src/components/button/__tests__/Button.spec.ts
@@ -1,11 +1,18 @@
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 
 import { Button } from "@/components/button";
 
 describe("Button", () => {
+  it("match snapshot", () => {
+    const wrapper = shallowMount(Button, {
+      slots: { default: "button" },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
   it("renders properly", () => {
-    const wrapper = mount(Button, {
+    const wrapper = shallowMount(Button, {
       slots: { default: "button" },
     });
     expect(wrapper.text()).toContain("button");
